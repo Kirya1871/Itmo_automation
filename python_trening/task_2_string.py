b7f6bdd113f0b616165d92089d4e5a66b7f5cb47
timatidallas <dallas_84@mail.ru>
2023-04-21 20:45:48 +0300

    firstcummit

---
+++ python_trening/task_2_string.py
@@ -0,0 +1,15 @@
+s = "это строка"
+print(s + '\n')
+
+g = '''Многосторчная
+строка'''
+print(g + '\n')
+print('строки ' + ' можно ' + 'складывать')
+
+print('auto' * 3)
+
+print(len('test'))
+
+a = 'auto'
+print(a[0])#a
+print(a[-2])#t
