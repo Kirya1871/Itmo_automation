a4151b491cd6e9281b677a5e90651bd292edbe95
timatidallas <dallas_84@mail.ru>
2023-05-27 17:22:43 +0300

    firstcummit

--- tests/test_one.py
+++ tests/test_one.py
@@ -3,3 +3,18 @@ def test_passing():
 def passing():
     print("hi")
 passing()
+
+def add (x, y):
+    return x+y
+print(add(1, 2))
+
+def arg(a, b, c=2, d=3):
+    return a+b+c+d
+print(arg(1, 1))
+
+
+def range_arg(a, b, c, d):
+    return a + ' ' + b + ' ' + c + ' ' + d + ' '
+print(range_arg('1', '6', '9', '4'))
+print(range_arg('1', b='500', d='50', c='212'))
+print(range_arg('a', 'b', 'c', 'd'))
